b0c4ec4920d10c25d8a9f8e1d36ccaa10f91eee4
LAB <luoanb@163.com>
2023-03-26 22:48:59 +0800

    更新版本

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,26 @@
-import path from 'path';
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import svgPlugin from "./svgconfig"
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import svgPlugin from "./svgconfig";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgPlugin],
   base: "./",
   esbuild: {
-    loader: 'tsx',
+    loader: "tsx",
     include: [/src\/.*\.[tj]sx?$/, /src\/.*\.png?$/],
   },
   optimizeDeps: {
-    include: ['/src'],
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx',
-      },
-    }
+    // include: [
+    //   "mui-form-hook",
+    //   "mui-layout-component",
+    //   "ref-component",
+    //   "react",
+    // ],
+    // esbuildOptions: {
+    //   loader: {
+    //     '.js': 'jsx',
+    //   },
+    // }
   },
-})
+});
