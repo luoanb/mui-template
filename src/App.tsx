6e22fb9bccd1053452ae8e7e14e84bc572680799
luoanbing <luoanb@163.com>
2023-10-19 07:24:05 +0000

    更新布局

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,16 @@
+import { useThemeContext } from "mui-layout-component";
 import { Router } from "./page/router";
+import { useMounted } from "./utils/hook";
+import { PureLightTheme } from "./design/PureLightTheme";
 
-export default Router;
+export default function App() {
+  const { setThemeByOptions } = useThemeContext();
+  useMounted(() => {
+    // 设置默认主题
+    // setThemeByOptions(PureLightTheme);
+    setThemeByOptions({
+      palette: { mode: "dark" },
+    });
+  });
+  return <Router />;
+}
--- src/page/routerLayout.tsx
+++ src/page/routerLayout.tsx
@@ -2,22 +2,54 @@ import { Container } from "@mui/material";
 import { Dashboard } from "mui-layout-component";
 import { Outlet } from "react-router-dom";
 
-import { styled } from "@mui/material/styles";
+import { Breakpoint, styled } from "@mui/material/styles";
 import Box, { BoxProps } from "@mui/material/Box";
 
 import Header from "../layout/header";
 import Nav from "../layout/nav";
 import { Design } from "../design/design";
 
-export const RouterDashboard = () => {
+export interface RouterDashboardProps {
+  /**
+   * 是否展示容器卡片
+   * @default true
+   * @type {boolean}
+   * @memberof RouterDashboardProps
+   */
+  withContainer?: boolean;
+  /**
+   * Container 最大宽度,
+   * @default lg
+   * @type {(false | Breakpoint | undefined)}
+   * @memberof RouterDashboardProps
+   */
+  maxWidth?: false | Breakpoint | undefined;
+  /**
+   * pspacing pading的t.spacing
+   * @default 4
+   * @type {number}
+   * @memberof RouterDashboardProps
+   */
+  pspacing?: number;
+}
+
+export const RouterDashboard = ({
+  withContainer = true,
+  maxWidth = "lg",
+  pspacing = 4,
+}: RouterDashboardProps) => {
   return (
     <Dashboard nav={<Nav />} header={<Header />} design={<Design />}>
-      <Container
-        maxWidth="lg"
-        sx={(theme) => ({ p: theme.spacing(4), height: "100%" })}
-      >
+      {withContainer ? (
+        <Container
+          maxWidth={maxWidth}
+          sx={(theme) => ({ p: theme.spacing(pspacing), height: "100%" })}
+        >
+          <Outlet />
+        </Container>
+      ) : (
         <Outlet />
-      </Container>
+      )}
     </Dashboard>
   );
 };
